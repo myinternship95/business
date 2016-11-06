d06153a9c4049c7bfd4e95e0255f9fbf83dd5f87
thanhbinh095 <thanhbinh0995@gmail.com>
2016-11-07 00:48:55 +0700

    home page

--- js/main.js
+++ js/main.js
@@ -1,82 +1,15 @@
 jQuery(document).ready(function() {
-	var e = $(".portfolio-grid");
-	e.isotope({
-	    itemSelector: ".portfolio-item",
-	    layoutMode: 'fitRows',
-	    animationOptions: {
-	        duration: 750,
-	        easing: "linear",
-	        queue: !1
-	    }
-	}), 
-	$(".portfolio-filter").on("click", ".categories", function() {
-	    var a = $(this).attr("data-filter");
-	    e.isotope({
-	        filter: a
-	    })
-	}), 
-	$(".categories-filter").each(function(a, e) {
-	    var i = $(e);
-	    i.on("click", ".categories", function() {
-	        i.find(".active").removeClass("active"), $(this).addClass("active")
-	    })
-	})
-
-	// $('.counter-num').each(function () {
-	//     $(this).prop('Counter',0).animate({
-	//         Counter: $(this).text()
-	//     }, {
-	//         duration: 5000,
-	//         easing: 'swing',
-	//         step: function (now) {
-	//             $(this).text(Math.ceil(now));
-	//         }
-	//     });
-	// });
-	// jQuery('.skillbar').each(function(){
-	// 	jQuery(this).find('.skillbar-bar').animate({
-	// 		width:jQuery(this).attr('data-percent')
-	// 	},6000);
-	// });
-	function bar() {
-		jQuery('.skillbar').each(function(){
-			jQuery(this).find('.skillbar-bar').animate({
-				width:jQuery(this).attr('data-percent')
-			},6000);
-		});
-	}
-	function count() {
-		$('.counter-num').each(function () {
-		    $(this).prop('Counter',0).animate({
-		        Counter: $(this).text()
-		    }, {
-		        duration: 5000,
-		        easing: 'swing',
-		        step: function (now) {
-		            $(this).text(Math.ceil(now));
-		        }
-		    });
-		});
-	}
-	var $section = $('#specialization');
-	$(document).bind('scroll', function(ev) {
-		var scrollOffset = $(document).scrollTop();
-        var containerOffset = $section.offset().top - window.innerHeight;
-        if (scrollOffset > containerOffset) {
-			bar();
-		}
-	});
-	// $(document).unbind('scroll');
-	var $section1 = $('#counter');
-	$(document).bind('scroll', function(ev) {
-		var scrollOffset1 = $(document).scrollTop();
-        var containerOffset1 = $section1.offset().top - window.innerHeight;
-        if (scrollOffset1 > containerOffset1) {
-			count();
-		}
-	});
-
- 	
 
 });
-
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	} else {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	}
+})
